b6634864b048ab4cc3848bf56e14eda5e3d1297f
Anca200 <anca2k17@gmail.com>
2024-04-13 02:57:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Batman");
+    searchMovies("Spider man");
   }, []);
 
   const searchMovies = async (title) => {
@@ -24,7 +24,7 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>MovieLand</h1>
+      <h1>Movies</h1>
 
       <div className="search">
         <input
